363428e521fbc539e507cb1e0949e3eb47974064
Nexlson <nelsonchoon112@gmail.com>
2022-01-05 11:01:10 +0800

    update comet

--- comet/experiment.py
+++ comet/experiment.py
@@ -1,23 +1,30 @@
 from comet_ml import Experiment 
 from numpy.random import permutation
 from sklearn import svm, datasets
+from keys import COMET_API
 
 def main():
-    # Initiate 
-    experiment = Experiment(api_key="fganx55aE3umFmx5iBWiuckxc", project_name="Iris SVM", workspace="nexlson")
+    # Comet Setup
+    experiment = Experiment(api_key=COMET_API, project_name="Iris SVM", workspace="nexlson")
 
-    C = 1.0
-    gamma = 0.8
+    # Hyperparams 
+    C = 0.8
+    gamma = 1.0
+
+    # Dataset preps
     iris = datasets.load_iris()
     per = permutation(iris.target.size)
     iris.data = iris.data[per]
     iris.target = iris.target[per]
+
+    # Model 
     clf = svm.SVC(C=C, kernel='rbf', gamma=gamma)
     clf.fit(iris.data[:90],
           iris.target[:90])
     score = clf.score(iris.data[90:],
                    iris.target[90:])
     
+    # Data logs
     experiment.log_parameters({'C': C, 'gamma': gamma})
     experiment.log_metrics({'score': score})
 
